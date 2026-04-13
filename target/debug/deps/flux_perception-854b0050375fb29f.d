31e659a0c9cfbc9270d8c7d92b2d10cd216e6dc9
Super Z <superz@flux.fleet>
2026-04-13 04:00:57 +0000

    Add .gitignore, remove tracked target/

--- target/debug/deps/flux_perception-854b0050375fb29f.d
+++
@@ -1,7 +0,0 @@
-/home/z/my-project/workspace/flux-perception/target/debug/deps/flux_perception-854b0050375fb29f.d: src/lib.rs
-
-/home/z/my-project/workspace/flux-perception/target/debug/deps/libflux_perception-854b0050375fb29f.rlib: src/lib.rs
-
-/home/z/my-project/workspace/flux-perception/target/debug/deps/libflux_perception-854b0050375fb29f.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/flux_perception-99d44f2ec918925d.d
+++
@@ -1,5 +0,0 @@
-/home/z/my-project/workspace/flux-perception/target/debug/deps/flux_perception-99d44f2ec918925d.d: src/lib.rs
-
-/home/z/my-project/workspace/flux-perception/target/debug/deps/flux_perception-99d44f2ec918925d: src/lib.rs
-
-src/lib.rs:
